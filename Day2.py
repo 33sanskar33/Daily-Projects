c8fc8d301a05fedff6bbc694711a39322c6ebc2e
33sanskar33 <famboysam@gmail.com>
2026-07-06 23:52:42 +0530

    Day 2: ATM Mini Project

---
+++ Day2.py
@@ -0,0 +1,12 @@
+#Day 2 Project: ATM Machine 
+
+print("Welcome to the ATM Machine")
+print("1.Check Balance")
+print("2.Withdraw Money")
+print("3.Deposit Money")
+print("4.Exit")
+
+choice = int(input("Enter Your Choice: "))
+
+if(choice == 1):
+    print("Balance is rs10000")
