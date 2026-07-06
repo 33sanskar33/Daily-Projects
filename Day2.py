b127431b7b8c0c97c6e0d20fba344a5b63611725
33sanskar33 <famboysam@gmail.com>
2026-07-07 00:28:54 +0530

    Project Done

--- Day2.py
+++ Day2.py
@@ -2,11 +2,33 @@
 
 print("Welcome to the ATM Machine")
 print("1.Check Balance")
-print("2.Withdraw Money")
-print("3.Deposit Money")
+print("2.Deposit Money")
+print("3.Withdraw Money")
 print("4.Exit")
+balance = 10000
 
 choice = int(input("Enter Your Choice: "))
 
 if(choice == 1):
-    print("Balance is rs10000")
+    print("Balance is: ", balance)
+elif(choice == 2):
+    deposit = int(input("Enter amount to deposit: "))
+    valid = 100
+    if(deposit > valid):
+        balance += deposit
+        print("₹", deposit, "Deposit Sucessfully")
+        print("Current Balance:", balance)
+    else:
+        print("Invalid Choiec")
+elif(choice == 3):
+    withdraw = int(input("Enter amount to withdraw: "))
+    if(balance > withdraw):
+        balance -= withdraw
+        print("Current balance is : ", balance)
+    else:
+        print("Insufficient Balance")
+elif(choice == 4):
+    print("Thanks for using ATM")
+else:
+    print("Invalid Choice")
+
